fe05731a3f63d14424e3ae7f8cd73f0ea7f17fcc
Benjamin Hitov <Benjamin.Hitov@gmail.com>
2012-05-17 21:49:07 -0400

    added sample settings file

---
+++ datadriven/sample_local_settings.py
@@ -0,0 +1,18 @@
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'ENTER SECRET KEY HERE'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'datadriven',                      # Or path to database file if using sqlite3.
+        'USER': 'ben',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+
+
+
+
